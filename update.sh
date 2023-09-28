f5539b9b2f2bd693016c5aba3215f2787ed1b211
yashiraan <144460948+yashiraan@users.noreply.github.com>
2023-09-28 16:24:04 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/Hermananza/vip/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/yashiraan/vip/main/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
